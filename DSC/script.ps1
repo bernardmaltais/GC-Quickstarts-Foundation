5255625f6c8013c83c832caae2ea68068f8ed9c0
Bernard <bernard@ducourier.com>
2019-04-04 12:29:52 -0400

    Add commands to open rdp access on 33890

--- DSC/script.ps1
+++ DSC/script.ps1
@@ -9,6 +9,10 @@ Process {
    $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
  }
 
+#Add RDP listening ports
+netsh interface portproxy add v4tov4 listenport=33890 connectport=3389 connectaddress=127.0.0.1 
+netsh advfirewall firewall add rule name="Open Port 33890" dir=in action=allow protocol=TCP localport=33890
+
 #Importing all required functions
 . $scriptPath\VSCodeSetup-x64-1.32.3.exe /VERYSILENT /MERGETASKS=!runcode
 
