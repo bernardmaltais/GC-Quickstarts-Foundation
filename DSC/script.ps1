79a9983e10438d3e8854e734732f42835536bbd1
Stefane Dumont <DumontS@mgmt.azpws01.pspc.gc.ca.local>
2019-04-09 13:24:12 -0400

    added disable of ie ahanced security and azure alias

--- DSC/script.ps1
+++ DSC/script.ps1
@@ -60,7 +60,16 @@ Install-PackageProvider -name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az -AllowClobber -force 
 $temptime = get-date -f yyyy-MM-dd--HH:mm:ss
 "After Installing AZ Powershell Module - $temptime" | out-file $deploylogfile -Append
+#enable azure alias
+Enable-AzureRmAlias -Scope LocalMachine
 #setting the time zone to eastern
 & "$env:windir\system32\tzutil.exe" /s "Eastern Standard Time"
 #writeToLog -message "StefaneMessage"
+#disable IE enhache Security
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" 
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" 
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 
+Stop-Process -Name Explorer 
+
 }
