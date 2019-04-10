43496684192daf22a7fbe3d85822ef1a9231e702
Stefane Dumont <DumontS@mgmt.azpws01.pspc.gc.ca.local>
2019-04-10 13:24:36 -0400

    debug 2

--- DSC/script.ps1
+++ DSC/script.ps1
@@ -53,7 +53,7 @@ Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 
 Stop-Process -Name Explorer 
 #copy batch file to install VSC extention on the public desktop 
-<#
+
 Copy-Item -Path $scriptpath\InstallVSCExtensions.bat -Destination C:\Users\Public\Desktop\InstallVSCExtensions.bat
 #adding a VSC shortcut on the public desktop
 $WshShell = New-Object -comObject WScript.Shell
@@ -61,6 +61,7 @@ $Shortcut = $WshShell.CreateShortcut("c:\Users\Public\Desktop\Visual Studio Code
 $Shortcut.TargetPath = "C:\Program Files\Microsoft VS Code\Code.exe"
 $Shortcut.Save()
 #adding Azure Deployment Library shortcut on the desktop
+<#
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("c:\Users\Public\Desktop\Azure Deployment Librairies.lnk")
 $Shortcut.TargetPath = "https://dev.azure.com/GC-Quickstarts/_git/Azure-Deployments"
