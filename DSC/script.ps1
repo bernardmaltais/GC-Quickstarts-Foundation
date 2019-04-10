cb296b48a56e97942d6b1a3e22b7e2673a976298
Stefane Dumont <DumontS@mgmt.azpws01.pspc.gc.ca.local>
2019-04-10 15:48:48 -0400

    test

--- DSC/script.ps1
+++ DSC/script.ps1
@@ -28,64 +28,6 @@ if ($rdpPort -ne 3389) {
 #Install stuff
 
 $temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"Installing Visual Studio - $temptime" | out-file $deploylogfile
-. $scriptPath\VSCodeSetup-x64-1.32.3.exe /VERYSILENT /MERGETASKS=!runcode
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"After Installing Visual Studio - $temptime" | out-file $deploylogfile -Append
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"Installing Chrome - $temptime" | out-file $deploylogfile -Append
-. $scriptPath\ChromeStandaloneSetup64.exe /silent /install
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"After Installing Chrome - $temptime" | out-file $deploylogfile -Append
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"Installing Git - $temptime" | out-file $deploylogfile -Append
-. $scriptPath\Git-2.21.0-64-bit.exe /silent
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"After Installing Git - $temptime" | out-file $deploylogfile -Append
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"Installing AZcopy - $temptime" | out-file $deploylogfile -Append
-. msiexec.exe /i $scriptPath\MicrosoftAzureStorageAzCopy_netcore_x64.msi /q /log $scriptPath\autoazcopyinstall.log
-#add the AZCOPY path to the path variable
-$AZCOPYpath = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
-$actualPath = ((Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path)
-$NEWPath =   "$actualPath;$AZCOPYpath"
-$NEWPath | Out-File $scriptPath\azcopySystemPath.log
-Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH -Value $NEWPath
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"After Installing AZcopy - $temptime" | out-file $deploylogfile -Append
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"Installing AZ Powershell Module - $temptime" | out-file $deploylogfile -Append
-#install Powershell AZ module
-Install-PackageProvider -name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name Az -AllowClobber -force 
-$temptime = get-date -f yyyy-MM-dd--HH:mm:ss
-"After Installing AZ Powershell Module - $temptime" | out-file $deploylogfile -Append
-#enable azure alias
-Enable-AzureRmAlias -Scope LocalMachine
-#setting the time zone to eastern
-& "$env:windir\system32\tzutil.exe" /s "Eastern Standard Time"
-#writeToLog -message "StefaneMessage"
-#disable IE enhache Security
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" 
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" 
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 
-Stop-Process -Name Explorer 
-#copy batch file to install VSC extention on the public desktop
-<#Copy-Item -Path $scriptpath\InstallVSCExtensions.bat -Destination C:\Users\Public\Desktop\InstallVSCExtensions.bat
-#adding a VSC shortcut on the public desktop
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("c:\Users\Public\Desktop\Visual Studio Code.lnk")
-$Shortcut.TargetPath = "C:\Program Files\Microsoft VS Code\Code.exe"
-$Shortcut.Save()
-#adding Azure Deployment Library shortcut on the desktop
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("c:\Users\Public\Desktop\Azure Deployment Librairies.lnk")
-$Shortcut.TargetPath = "https://dev.azure.com/GC-Quickstarts/_git/Azure-Deployments"
-$Shortcut.IconLocation="$scriptPath\ADLicon.ico"
-$Shortcut.Save()
-#disable server manager at login time
-Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose 
-#>
+
 }
 
